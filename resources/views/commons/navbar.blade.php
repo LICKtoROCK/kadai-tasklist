17ca13f6ff10d511c4e2021e27f18e16950af43c
kenya.kitagawa <lick.to.rock@gmail.com>
2022-06-20 13:56:25 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,20 +1,23 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <a class="navbar-brand" href="/">TaskList</a>
-
+        
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
-            </ul>
+            @if(Auth::check()) 
+                <ul class="navbar-nav">
+                    <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿',[],['class'=>'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('logout.get', 'ログアウト',[],['class'=>'nav-link']) !!}</li>
+                </ul>
+            @else
+                <ul class="navbar-nav">
+                    <li class="nav-item">{!! link_to_route('signup.get','サインアップ',[],['class'=>'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login','ログイン',[],['class'=>'nav-link']) !!}</li>
+                </ul>
+            @endif
         </div>
-        <ul class="navbar-nav">
-            <li>{!! link_to_route('signup.get','Signup',[],['class'=>'nav-link']) !!}</li>
-            <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-        </ul>
     </nav>
 </header>
